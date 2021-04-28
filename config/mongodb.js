06cd418fdd4e26b9044e6a72169748dc37cd22c2
ygorverde <ygorverde572@gmail.com>
2021-04-28 14:58:44 -0300

    .

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://deploy:uploaddeploy@cluster0.h3vg0.mongodb.net/knowledge_stats?retryWrites=true&w=majority', { useNewUrlParser: true })
     .catch(e => {
         const msg = 'ERRO!! Não foi possível conectar com o MongoDB'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
